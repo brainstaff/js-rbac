163ff48e602b9359d7a8d4f4510eacf486f22b12
Victor Dombrovskiy <dombrovskiy@klaim.ai>
2024-11-28 18:15:43 +0700

    Add tests for checking access in context.

--- packages/rbac/test/test.ts
+++ packages/rbac/test/test.ts
@@ -3,6 +3,7 @@ import assert from 'assert';
 import { RbacInMemoryAssignmentAdapter, RbacInMemoryItemAdapter, RbacInMemoryItemChildAdapter } from '@brainstaff/rbac-in-memory'
 
 import { RbacAdapter, RbacManager } from '../src/index.js';
+import { stringify } from '../src/utils.js';
 
 const $ = {
   a: "a",
@@ -16,10 +17,23 @@ const $ = {
     isOwnProfile: "isOwnProfile",
     updateProfile: "updateProfile",
   },
+  contextId: {
+    a: {
+      product: "A",
+      customer: "1",
+    },
+    b: {
+      product: "B",
+      customer: "1",
+    },
+  },
 }
 
 const createManager = async () => {
-  type RbacContextId = never;
+  type RbacContextId = {
+    product: string,
+    customer: string,
+  };
   type RbacRulePayload = {
     me?: string,
     owner?: string,
@@ -77,30 +91,40 @@ const createManager = async () => {
 describe('RbacManager', function() {
   it('admin', async () => {
     const m = await createManager();
-    assert.equal(await m.check($.a, $.item.admin), true, `should have role ${$.item.admin}`);
-    assert.equal(await m.check($.a, $.item.manager), true, `should have role ${$.item.manager}`);
-    assert.equal(await m.check($.a, $.item.user), true, `should have role ${$.item.user}`);
+    assert.equal(await m.check($.a, $.item.admin), true, `should be ${$.item.admin}`);
+    assert.equal(await m.check($.a, $.item.manager), true, `should be ${$.item.manager}`);
+    assert.equal(await m.check($.a, $.item.user), true, `should be ${$.item.user}`);
     assert.equal(await m.check($.a, $.item.updateProfile, { me: $.a, owner: $.a }), true, `should be able to ${$.item.updateProfile} of ${$.a}`);
     assert.equal(await m.check($.a, $.item.updateProfile, { me: $.a, owner: $.m }), true, `should be able to ${$.item.updateProfile} of ${$.m}`);
   });
 
   it('manager', async () => {
     const m = await createManager();
-    assert.equal(await m.check($.m, $.item.admin), false, `should not have role ${$.item.admin}`);
-    assert.equal(await m.check($.m, $.item.manager), true, `should have role ${$.item.manager}`);
-    assert.equal(await m.check($.m, $.item.user), true, `should have role ${$.item.user}`);
-    assert.equal(await m.check($.m, $.item.trololo), false, `should not have role ${$.item.trololo}`);
+    assert.equal(await m.check($.m, $.item.admin), false, `should not be ${$.item.admin}`);
+    assert.equal(await m.check($.m, $.item.manager), true, `should be ${$.item.manager}`);
+    assert.equal(await m.check($.m, $.item.user), true, `should be ${$.item.user}`);
+    assert.equal(await m.check($.m, $.item.trololo), false, `should not be ${$.item.trololo}`);
     assert.equal(await m.check($.m, $.item.updateProfile, { me: $.m , owner: $.m }), true, `should be able to ${$.item.updateProfile} of ${$.m}`);
     assert.equal(await m.check($.m, $.item.updateProfile, { me: $.m , owner: $.a }), false, `should not be able to ${$.item.updateProfile} of ${$.a}`);
   });
 
   it('user', async () => {
     const m = await createManager();
-    assert.equal(await m.check($.u, $.item.manager), false, `should not have role ${$.item.manager}`);
+    assert.equal(await m.check($.u, $.item.manager), false, `should not be ${$.item.manager}`);
     await m.assign({ userId: $.u, role: $.item.manager });
-    assert.equal(await m.check($.u, $.item.manager), true, `should have role ${$.item.manager}`);
+    assert.equal(await m.check($.u, $.item.manager), true, `should be ${$.item.manager}`);
     await m.revoke($.u, $.item.manager);
-    assert.equal(await m.check($.u, $.item.manager), false, `should have role ${$.item.manager} revoked`);
+    assert.equal(await m.check($.u, $.item.manager), false, `should be ${$.item.manager} revoked`);
     assert.rejects(m.assign({ userId: $.u, role: $.item.trololo }), { name: 'Error', message: `No such role ${$.item.trololo}.`});
+  });
+
+  it(`user in context`, async () => {
+    const m = await createManager();
+    assert.equal(await m.check($.u, $.item.manager), false, `${stringify(undefined)}: should not be ${$.item.manager}`);
+    assert.equal(await m.check($.u, $.item.manager, { contextId: $.contextId.a }), false, `${stringify($.contextId.a)}: should not be ${$.item.manager}`);
+    await m.assign({ userId: $.u, role: $.item.manager, contextIds: [$.contextId.a] });
+    assert.equal(await m.check($.u, $.item.manager), false, `${stringify(undefined)}: should not be ${$.item.manager}`);
+    assert.equal(await m.check($.u, $.item.manager, { contextId: $.contextId.a }), true, `${stringify($.contextId.a)}: should be ${$.item.manager}`);
+    assert.equal(await m.check($.u, $.item.manager, { contextId: $.contextId.b }), false, `${stringify($.contextId.b)}: should not be ${$.item.manager}`);
   });
 });
